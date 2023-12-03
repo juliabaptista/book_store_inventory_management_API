f10bff16d0aa09e91f78199b161bb37e721542da
juliabaptista <juliabaptistadeoliveira@gmail.com>
2023-12-03 22:10:03 +0100

    feat: src/model - create Book class

---
+++ src/main/java/org/example/model/Book.java
@@ -0,0 +1,68 @@
+package org.example.model;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private double price;
+    private int quantity;
+
+    public Book(int id,String title, String author, double price, int quantity) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
