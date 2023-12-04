6a4bd6f2c051cd7432c89730d975fa680a6131bd
juliabaptista <juliabaptistadeoliveira@gmail.com>
2023-12-04 11:47:38 +0100

    feat: add sout for CRUD operations and connections + call close connection method

--- src/main/java/org/example/util/TestDBConnection.java
+++ src/main/java/org/example/util/TestDBConnection.java
@@ -8,12 +8,14 @@ import java.sql.Connection;
 public class TestDBConnection {
     public static void main(String[] args) {
         Connection connection = DBConnection.openConnection();
+        System.out.println("Connected");
 
 //        db CRUD operations
         Book book5 = new Book(5,"Book 5", "Author 5", 59.99, 50);
         BookDAO.insertBook(connection, book5);
         System.out.println("Insertion completed");
 
-//        DBConnection.closeConnection();
+        DBConnection.closeConnection();
+        System.out.println("Connection closed");
     }
 }
